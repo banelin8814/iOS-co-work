9e5993f1499864ef367b525b1dc3335712a7afd1
NY <ib310519@gmail.com>
2024-03-29 20:35:03 +0800

    add banner with four images in LobbyView

---
+++ Stylish-iOS-co-work/STYLiSH/Views/Lobby/LobbyBanner.swift
@@ -0,0 +1,21 @@
+//
+//  LobbyBanner.swift
+//  STYLiSH
+//
+//  Created by NY on 2024/3/29.
+//  Copyright © 2024 AppWorks School. All rights reserved.
+//
+
+import UIKit
+
+class LobbyBanner: UITableViewHeaderFooterView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
