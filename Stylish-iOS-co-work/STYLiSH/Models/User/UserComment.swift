8da75ba28351bec0f816629bf2b5f1dd00116520
Kyle Lu <nbnb6712@gmail.com>
2024-03-31 14:18:52 +0800

    fix: add UserComment Struct

---
+++ Stylish-iOS-co-work/STYLiSH/Models/User/UserComment.swift
@@ -0,0 +1,14 @@
+//
+//  UserComment.swift
+//  STYLiSH
+//
+//  Created by Kyle Lu on 2024/3/31.
+//  Copyright © 2024 AppWorks School. All rights reserved.
+//
+
+import UIKit
+struct UserComment {
+    let username: String
+    let comment: String
+    let rating: Int
+}
